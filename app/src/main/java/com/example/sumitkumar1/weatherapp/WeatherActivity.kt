cbf9d9feb4978438abc7f20b5b4d839031c2afc9
YATRAONLINE\sumit.kumar1 <sumit.kuma1@yatra.com>
2018-05-05 12:39:36 +0530

    Initial changes

---
+++ app/src/main/java/com/example/sumitkumar1/weatherapp/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sumitkumar1.weatherapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
